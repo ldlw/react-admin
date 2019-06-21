296e64c3358f3684333b1a5a614dbbe94e83f9c5
liuwei <lvyx620@163.com>
2019-06-21 18:50:54 +0800

    测试login路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React,{ Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Login from './pages/login';
+// import Admin from './pages/admin';
 export default class App extends Component {
   render() {
     return <Switch>
       <Route path='/login' component={Login}/>
-      <Route path='/' component={Admin}/>
+      {/*<Route path='/' component={Admin}/>*/}
     </Switch>
   }
 }
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -8,7 +8,7 @@ import logo from './logo.png';
 //缓存一下
 const Item= Form.Item;
 
-class  Login extends Component {
+export default class  Login extends Component {
 
   render() {
 
@@ -20,3 +20,7 @@ class  Login extends Component {
     </div>
   }
 }
+
+//返回值是一个包装组件 <Form(Login)><Login><Form(Login)>
+//通过Form(Login)包装组件向Login组件中传递form属性
+// export default Form.create()(Login);
