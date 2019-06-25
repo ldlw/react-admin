341b9775ddbe9feb8834feb640c8438ff97b9a9a
liuwei <lvyx620@163.com>
2019-06-25 20:03:04 +0800

    完成了添加品类的样式和功能

--- src/api/index.js
+++ src/api/index.js
@@ -7,8 +7,9 @@ export const reqLogin = (username, password) => ajax('/login', {username, passwo
 export const reqValidateUserInfo = (id) => ajax('/validate/user', {id}, 'POST');
 
 export const reqWeather = function () {
-  return new Promise((resolve,reject) =>{
-    jsonp(`http://api.map.baidu.com/telematics/v3/weather?location=深圳&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`, {},function (err,data){
+  let cancel = null;
+  const promise = new Promise((resolve,reject) =>{
+    cancel = jsonp(`http://api.map.baidu.com/telematics/v3/weather?location=深圳&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`, {},function (err,data){
       if(!err) {
         const {dayPictureUrl,weather } = data.results[0].weather_data[0];
         resolve({
@@ -20,5 +21,14 @@ export const reqWeather = function () {
         resolve();
       }
     })
-  })
-}
+  });
+
+  return {
+    promise,
+    cancel
+  }
+};
+
+export const reqCategories = (parentId) => ajax('/manage/category/list',{parentId});
+
+export const reqAddCategory = (parentId, categoryName) => ajax('/manage/category/add',{parentId,categoryName},'post');
--- src/pages/category/index.jsx
+++ src/pages/category/index.jsx
@@ -1,19 +1,71 @@
 import React,{ Component } from 'react';
-import { Card, Button, Icon, Table } from 'antd';
+import { Card, Button, Icon, Table, Modal,message } from 'antd';
 
+import { reqCategories, reqAddCategory } from '../../api'
 import MyButton from '../../components/my-button';
+import AddCategoryForm from './add-category-form';
 import './index.less'
+
+
 export default class Category extends Component {
   state = {
-    categories:[] //一级分类列表
+    categories:[], //一级分类列表
+    isShowAddCategory: false //显示添加品类
+  };
+
+  async componentDidMount() {
+    const result = await reqCategories('0');
+    // console.log(result);
+
+    if(result){
+      this.setState({categories:result});
+    }
   }
 
+  //显示添加品类
+  showAddCategory = () =>{
+    this.setState({
+      isShowAddCategory:true
+    })
+  };
+
+  //隐藏添加品类
+  hideAddCategory = () =>{
+    this.setState({
+      isShowAddCategory: false
+    })
+  };
+
+  //添加品类
+  addCategory = () =>{
+    //1.表单校验
+    //2.收集表单数据
+    this.addCategoryform.props.form.validateFields(async(err,values) =>{
+       if(!err){
+         //校验通过
+         // console.log(values)
+         const {parentId,categoryName} = values;
+         const result = await reqAddCategory(parentId,categoryName);
+         if(result){
+           //添加分类成功
+           message.success('添加分类成功~');
+           this.setState({
+             isShowAddCategory:false
+           })
+         }
+       }
+    })
+    //3.发送请求
+    // console.log(this.addCategoryform);//实例对象
+  };
+
   render() {
+    const {categories,isShowAddCategory} = this.state;
     const columns = [
       //决定表头内容
       {
         title: '品类名称',
-        dataIndex: 'categoryName',
+        dataIndex: 'name',
       },
       {
         title: '操作',
@@ -26,7 +78,7 @@ export default class Category extends Component {
       },
     ];
     //决定表格的数据
-    const data = [
+   /* const data = [
       {
         key: '1',
         categoryName: '手机',
@@ -47,12 +99,12 @@ export default class Category extends Component {
         categoryName: '鼠标',
         // operation: 'zzz',
       },
-    ];
+    ];*/
 
-    return <Card title="一级分类列表" extra={<Button type="primary"><Icon type="plus"/>添加品类</Button>}>
+    return <Card title="一级分类列表" extra={<Button type="primary" onClick={this.showAddCategory}><Icon type="plus"/>添加品类</Button>}>
       <Table
         columns={columns}
-        dataSource={data}
+        dataSource={categories}
         bordered
         pagination={{
           showSizeChanger: true,
@@ -60,8 +112,19 @@ export default class Category extends Component {
           defaultPageSize: 3,
           showQuickJumper: true
         }}
-
+        rowKey="_id"
       />
+      <Modal
+        title="添加分类"
+        visible={isShowAddCategory }
+        onOk={this.addCategory}
+        onCancel={this.hideAddCategory}
+        okText="确认"
+        cancelText="取消"
+      >
+        {/*第一个form是AddCategoryForm 第二个form是当前Category的实例对象上 addCategoryform*/}
+        <AddCategoryForm categories={categories} wrappedComponentRef={(form) => this.addCategoryform = form}/>
+      </Modal>
     </Card>
   }
 }
